c4b5fc0eec5afb9fdcf6a89541a94b1a6d641d15
Khoa Pham <onmyway133@gmail.com>
2018-07-04 18:04:52 +0200

    Add MathService

---
+++ Sources/MathService.swift
@@ -0,0 +1,13 @@
+//
+//  MathService.swift
+//  MathSolver
+//
+//  Created by Khoa Pham on 04.07.2018.
+//  Copyright © 2018 onmyway133. All rights reserved.
+//
+
+import Foundation
+
+class MathService {
+  
+}
